2af6a27639333f69a0f431e7c539bb7dc89b40c3
Imrann John <life.imrann@gmail.com>
2021-11-08 08:41:32 +0100

    Nouvelle version arrayList fonctionnel

---
+++ src/MaListe.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class MaListe {
+	ArrayList<Integer> maliste = new ArrayList<Integer>() ;
+	
+	public ArrayList<Integer> ajouterValeur(int valeur){
+		this.maliste.add(valeur);
+		System.out.println(maliste);
+		System.out.println("La taille de ma liste: " + maliste.size());
+		return maliste;
+	}
+	
+
+	public static void main(String[] args) {
+		MaListe maliste = new MaListe();
+		maliste.ajouterValeur(5);
+		maliste.ajouterValeur(4);
+		maliste.ajouterValeur(3);
+		
+		
+	}
+}
